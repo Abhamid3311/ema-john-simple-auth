6b70c22384ad504dca043edea97ad91050d2ae9e
Hamid <www.abdulhamid3311@gmail.com>
2022-03-23 17:43:18 +0600

    added cart balances

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import './Cart.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const Cart = (props) => {
+
+    const { cart } = props;
+    console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total += product.price;
+        shipping += product.shipping;
+    }
+    let tax = +(total * 0.1).toFixed(2);
+
+    let grandTotal = total + shipping + tax;
+
+
+
+
     return (
-        <div className='cart-container'>
+        <div className='cart'>
             <h2>Cart Summary</h2>
-            <h3>Selected Item: {props.cart.length}</h3>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total price: ${total}</p>
+            <p>Total shipping charge: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h2>Grand Total: ${grandTotal}</h2>
+            <div >
+                <button className='btn-remove'>
+                    <p>Clear Cart</p>
+                    <FontAwesomeIcon icon={faTrashAlt} />
+                </button>
+                <button className='btn-review'>
+                    <p>Review Order</p>
+                    <FontAwesomeIcon icon={faArrowRight} />
+
+                </button>
+            </div>
+
         </div>
     );
 };
