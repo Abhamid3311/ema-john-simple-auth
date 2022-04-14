147d03a973b65295ab5e2fed51284fa39f6fe3a9
Hamid <www.abdulhamid3311@gmail.com>
2022-04-14 19:32:27 +0600

    added login form

---
+++ src/components/Login/Login.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+
+const Login = () => {
+    return (
+        <div className='form-container'>
+            <div>
+                <h2 className='form-titel'>Login</h2>
+                <form action="">
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="" />
+                    </div>
+                    <input className='form-submit' type="submit" value="Login" />
+                </form>
+                <p>
+                    New to Ema-John? <Link className='form-link' to='/signup'>create an account</Link>
+                </p>
+            </div>
+
+        </div>
+    );
+};
+
+export default Login;
