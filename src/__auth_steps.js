8dd13471e2d4a23b0ace83ab1110af0b3c7c5ff4
Hamid <www.abdulhamid3311@gmail.com>
2022-04-15 10:18:04 +0600

    implement email-pass login and signin

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -1,6 +1,12 @@
 /* 
-1.Create a new Project in console.google.com
+1.Create a new firebase Project in console.google.com
 2.npm install firebase
 3. copy-paste firebase.init.js, add const auth and export auth;
 4. firebase settings > Authentication> enable Email-password auth
+5. crearte signin,Login compo, setup route
+6. attach form field handler and submit handler
+7. npm install --save react-firebase-hooks
+8. useCreateUserWithEmailAndPassword(auth) from react-firebase-hooks
+9. if user is created redirect to the expected page
+10.  useSignInWithEmailAndPassword(auth);
 */
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    const handleEmailBlur = (e) => {
+        setEmail(e.target.value);
+    };
+    const handlePasswordBlur = e => {
+        setPassword(e.target.value);
+    };
+    const handleConfirmPasswordBlur = e => {
+        setConfirmPassword(e.target.value);
+    };
+
+    if (user) {
+        navigate('/');
+    }
+    const handleCreateUsers = e => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            setError('your two password did not match!');
+            return;
+        }
+        if (password.length < 6) {
+            setError('Password must be 6 charecters or longer');
+            return;
+        }
+        createUserWithEmailAndPassword(email, password);
+    };
+
+
+
     return (
-        <div>
-            <h2>Sign Up</h2>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-titel'>Sign Up</h2>
+                <form onSubmit={handleCreateUsers}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" required />
+                    </div>
+                    <p style={{ color: "red" }}>{error}</p>
+                    <input className='form-submit' type="submit" value="Sign Up" />
+                </form>
+                <p className='forget-pass-p'>
+                    Already have an account? <Link className='form-link' to='/login'>Login</Link>
+                </p>
+                <button className='google-signIn-btn'>Continue with Google</button>
+            </div>
+
         </div>
     );
 };
